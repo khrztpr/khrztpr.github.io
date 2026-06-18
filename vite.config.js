506f40b526c34a36b59631175489cedd489dadcf
khrztpr <166202405+khrztpr@users.noreply.github.com>
2026-06-18 13:36:55 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/khrztpr.github.io/', 
+  base: '/', // This forces Vite to generate absolute paths from the root domain
 })
