246ea75c856c76371a52c06cec814003b4531f6d
khrztpr <166202405+khrztpr@users.noreply.github.com>
2026-06-19 09:43:50 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Changes paths to relative so it works perfectly on GitHub Pages root
-  build: {
-    outDir: 'dist', // Ensures the compiled files go into the folder our workflow uploads
-    assetsDir: 'assets', // Puts JS and CSS into a dedicated assets folder
-    rollupOptions: {
-      input: {
-        main: './index.html', // Forces Vite to explicitly find your index.html
-      },
-    },
-  },
+  base: '/khrztpr.github.io/', 
 })
