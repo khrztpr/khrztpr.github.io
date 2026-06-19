ba0a6ad1e9989a3438e4d3e1142b2ecc2ef514c1
khrztpr <166202405+khrztpr@users.noreply.github.com>
2026-06-18 18:03:52 -0700

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5wNeyT32d1UFO4jgOVJHTuZiZg0DsTLE",
+  authDomain: "test-46743.firebaseapp.com",
+  projectId: "test-46743",
+  storageBucket: "test-46743.firebasestorage.app",
+  messagingSenderId: "875142432837",
+  appId: "1:875142432837:web:d229701d4c605ace887b2c",
+  measurementId: "G-4H1HXQ7LW2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
